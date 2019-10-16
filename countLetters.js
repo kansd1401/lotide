2f32ff9a975476d9730e854d278a1b2de6ca68a6
kansd1401 <kang.sanjeet1401@gmail.com>
2019-10-16 21:23:36 +0000

    added asssert to countLetters

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,23 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(Object.keys(actual),Object.keys(expected))) {
+    console.log(`✅✅✅ The 2 arrays are equal: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 The 2 arrays are not equal: ${actual} !== ${expected}`);
   }
+  return;
 };
+
+const eqArrays = function(arr1, arr2){
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  for(var i=0;i<arr1.length;i++){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+}
 
 const countLetters = function(str) {
   let result = {};
@@ -22,4 +35,15 @@ const countLetters = function(str) {
   return result;
 };
 
-console.log(countLetters("lighthouse in the house"));
+assertArrayEqual(countLetters("lighthouse in the house"),{
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+});
