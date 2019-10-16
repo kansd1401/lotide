08a09b7e8079981f06ce348a044be78a0019fa0a
kansd1401 <kang.sanjeet1401@gmail.com>
2019-10-16 19:58:56 +0000

    added count function

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let result={};
+  for(var str in itemsToCount){
+    let count = 0;
+    for(var letter of allItems){
+      if(letter===str && itemsToCount[str]){
+        count++;
+      }
+    }
+    if (count === 0){
+      count = undefined;
+    }
+    result[str] = count;
+  }
+  return result;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
